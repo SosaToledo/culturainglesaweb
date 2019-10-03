ef0a989c8e432bd16bce71a086bcab282ea3bf89
luciano <luchososa1990@gmail.com>
2019-10-03 13:24:30 -0300

    init

---
+++ header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Asociación Pergamino de Cultura Inglesa</title>
+    <style>
+      header{
+        height: 80px;
+        position: relative;
+        background-color: white;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      header img{
+        border-radius: 50%
+      }
+      #logo{
+        position: absolute;
+        left: 8%;
+        top: 5px;
+      }
+      body{
+        margin: 0;
+        background-color: red;
+      }
+      header p{
+        margin: 0;
+        font-size: 2em
+      }
+    </style>
+</head>
+<body>
+  <header>  
+    <img id="logo" class="rounded-circle" style="width: 150px" src="<?php bloginfo('template_directory');?>/img/logocultura.jpg" alt="">
+    <p>ASOCIACIÓN PERGAMINO DE CULTURA INGLESA</p>
+  </header>
+  <section>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+    algo <br>
+  </section>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+  <!--CARROUSEL-->
+  <div></div>
+  <!--CIRCULOS DE CURSOS-->
+  <div></div>
+  <!--CURSOS INTERNACIONALES Y NOTICIAS-->
+  <div></div>
+</body>
+<?php get_footer(); ?>
+</html>
