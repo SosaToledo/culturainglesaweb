1c10e19415983ea8d3e5fe923040cd37adb82d7e
lucianoSosa <luchososa1990@gmail.com>
2020-05-29 18:22:28 -0300

    delete

--- header.php
+++ header.php
@@ -28,10 +28,39 @@
 
 <body style="height:100vh">
   <header id="header">
-    <a href="<?php echo get_site_url(); ?>">
-      <img id="logo" class="rounded-circle .d-none .d-sm-block" style="width: 150px" src="<?php bloginfo('template_directory'); ?>/img/logocultura.jpg" alt="">
-    </a>
-    <div id="title" class=".d-none .d-sm-block">
-      <p>Prueba de branch nuevodiseno</p>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo get_site_url(); ?>">Cultura Inglesa Pergamino</a>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0 pr-5 mr-5">
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo get_site_url(); ?>/Nosotros">Nosotros</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo get_site_url(); ?>/Cursos">Cursos</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo get_site_url(); ?>/Examenes">Examenes</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo get_site_url(); ?>/biblioteca">Biblioteca</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo get_site_url(); ?>/Novedades">Novedades</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="https://campus.apci.org.ar"><strong>CAMPUS</strong></a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo get_site_url(); ?>/404">Convenios</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo get_site_url(); ?>/Contacto">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
--- index.php
+++ index.php
@@ -1,32 +1,9 @@
 <?php get_header(); ?>
-<?php include 'menu.php'; ?>
   <!--CARROUSEL-->
   <div id="carrousel">
-    <img src="<?php bloginfo('template_directory');?>/img/portada2.jpg" alt="">
-  </div>
-  <!-- Segunda mitad del sitio -->
-  <div id="segundaMitadSitio" class="">
-    <!--CIRCLES-->
-    <div id="circlesParents" class="">
-      <div id="circles" style="">
-        <!--KID-->
-        <img id="" class="imgcircle circlesGrade" src="<?php bloginfo('template_directory');?>/img/kids.png" alt="Kids button">
-        <!--TEENS-->
-        <img id="" class="imgcircle circlesGrade" src="<?php bloginfo('template_directory');?>/img/teens.png" alt="Teens button">
-        <!--ADULTS-->
-        <img id="" class="imgcircle circlesGrade" src="<?php bloginfo('template_directory');?>/img/adults.png" alt="Adults button">
-      </div>
-      <div id="circles">
-      <!-- <div class="circlesGradeDectoration"></div>
-      <div class="circlesGradeDectoration"></div>
-      <div class="circlesGradeDectoration"></div> -->
-    </div>
-  </div>
-  <!--CURSOS INTERNACIONALES Y NOTICIAS-->
-  <div class="highlight">
     
   </div>
-</div>
+  <!--FOOTER-->
+  <?php get_footer(); ?>
 </body>
-<?php get_footer(); ?>
 </html>
