6d0485cb323d529b0dbde43c8a6e1d28dbefc8f5
lucianoSosa <luchososa1990@gmail.com>
2020-06-02 15:59:44 -0300

    cambios de index

--- header.php
+++ header.php
@@ -14,53 +14,37 @@
 
   <!--BOOTSTRAP-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
   <!--OWN-->
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/media.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
   <script src="<?php bloginfo('template_directory'); ?>/js/function.js"></script>
 
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
 </head>
 
-<body style="height:100vh">
+<body class="fluid-container px-4">
   <header id="header">
-    <nav class="navbar navbar-expand-lg navbar-light ">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo get_site_url(); ?>">Cultura Inglesa Pergamino</a>
-
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0 pr-5 mr-5">
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo get_site_url(); ?>/Nosotros">Nosotros</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo get_site_url(); ?>/Cursos">Cursos</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo get_site_url(); ?>/Examenes">Examenes</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo get_site_url(); ?>/biblioteca">Biblioteca</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo get_site_url(); ?>/Novedades">Novedades</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="https://campus.apci.org.ar"><strong>CAMPUS</strong></a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo get_site_url(); ?>/404">Convenios</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo get_site_url(); ?>/Contacto">Contacto</a>
-          </li>
+  <nav class="navbar navbar-expand-md fixed-top maine-menu">
+    <div class="container">
+      <button class="navbar-toggler ml-auto" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse"><span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
+      <div id="my-nav" class="collapse navbar-collapse">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Inicio</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Nosotros</a></li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Cursos</a></li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Examenes</a></li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Biblioteca</a></li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Novedades</a></li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Campus</a></li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Convenios</a></li>
+          <li class="nav-item"> <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Contacto</a></li>
         </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
   </header>
--- index.php
+++ index.php
@@ -1,7 +1,50 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Asociación Pergamino de Cultura Inglesa</title>
+
+  <!--BOOTSTRAP-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+  <!--OWN-->
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/media.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
+  <script src="<?php bloginfo('template_directory'); ?>/js/function.js"></script>
+</head>
+<body>
+  <!--MENU-->
+  <div class="container-fluid row align-items-center justify-content-center m-0" id="menu-card">
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Nosotros</div></div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Cursos</div></div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Examenes</div></div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Biblioteca</div></div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card d-none d-xl-block">
+      <div class="row align-items-center justify-content-center">
+        <img class="col-12" style="height: 20%" src="img/logocultura.jpg" alt="">
+      </div>
+    </div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Novedades</div></div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Campus</div></div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Convenios</div></div>
+    <div class="mx-xl-2 m-lg-1 col-xl-1 col-lg-5 card boder border-dark rounded-lg align-items-center justify-content-end"><div>Contacto</div></div>
+  </div>
   <!--CARROUSEL-->
   <div id="carrousel">
-    
+      
   </div>
   <!--FOOTER-->
   <?php get_footer(); ?>
